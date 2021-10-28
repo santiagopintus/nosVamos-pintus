ea74f5b69d79844c9b18f1137d7203de65f56ad0
Santiago Pintus <santiagopintus@gmail.com>
2021-10-28 20:08:25 -0300

    Agregué la navbar y los estilos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import NavBar from './components/Navbar/NavBar';
+import './components/Navbar/NavBar.css';
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
-      <button type="button" className="btn btn-primary">Click me</button>
+      <NavBar logo="images/logo192.png"/>
     </>
   );
 }
