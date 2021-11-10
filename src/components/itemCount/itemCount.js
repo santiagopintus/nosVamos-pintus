2c6b937eec38a1166dc2472fd1d56a9df1e9fb2a
Santiago Pintus <santiagopintus@gmail.com>
2021-11-10 17:01:05 -0300

    Funciona genial, solo tengo que borrar el cartel de éxito dsp de 3 segundos

--- src/components/itemCount/itemCount.js
+++ src/components/itemCount/itemCount.js
@@ -4,6 +4,10 @@ import './itemCount.css';
 const ItemCount = (props) => {
 
     let [count, setCount] = useState(props.initial);
+    let [addedToCart, setAddedToCart] = useState({
+        added: false,
+        quantity: 0,
+    });
 
     const increment = () => {
         if (props.stock > count && props.stock > 0) {
@@ -19,6 +23,7 @@ const ItemCount = (props) => {
     const reset = () => setCount(0);
 
     const onAdd = (quantity) => {
+        setAddedToCart({added: true, quantity: quantity});
         props.onAdd(quantity);
         reset();  
     };
@@ -35,13 +40,22 @@ const ItemCount = (props) => {
                 <button className="delete-icon" onClick={reset} title="Resetear item">
                     <img src="images/icons/delete_icon.svg" alt="Icono de resetear la cantidad del item seleccionado"></img>
                 </button>
-                <button className="add-icon" title="Agregar al carrito">
+                <button className="add-icon" title="Agregar al carrito"
+                    onClick={() => onAdd(count)}
+                    disabled={count === 0 || props.stock === 0}
+                >
                     <img src="images/icons/addToCart.svg" alt="Icono de agregar al carrito"></img>
                 </button>
                 
                 {
                     props.stock <= 0 &&
                     <p className="stock-error">Sin stock</p>
+                }
+
+                {
+                    addedToCart.added &&
+
+                    <p className="added-to-cart">{ addedToCart.quantity } items agregados al carrito</p>
                 }
 
             </div>
