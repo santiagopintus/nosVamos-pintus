a2be47d7670c00937a9734c10248ce65d05c5b46
Santiago Pintus <santiagopintus@gmail.com>
2021-11-15 15:17:14 -0300

    Agregué gulp para convertir muchas imagenes a webp

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+// import imagemin from 'gulp-imagemin';
+const { src, dest, parallel } = require('gulp');
+const webp = require('gulp-webp');
+
+const paths = {
+    imagenesJpg: 'resized jpg/**/*.{jpg,png}',
+    imagenesWebp: 'webp output',
+}
+
+// function compressImg() {
+//     return src(paths.imagenesJpg)
+//         .pipe(cache(imagemin({ optimizationLevel: 3})))
+//         .pipe(dest('build/img'))
+// }
+
+function convertToWebp() {
+    return src(paths.imagenesJpg)
+        .pipe( webp() )
+        .pipe(dest(paths.imagenesWebp))
+}
+
+// exports.default = parallel(convertToWebp);
