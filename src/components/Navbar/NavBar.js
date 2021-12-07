70cba2afe1f68d3d850f26401904524de60b7b08
Santiago Pintus <santiagopintus@gmail.com>
2021-12-07 00:16:07 -0300

    Actualizando favicon y logos

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -2,7 +2,7 @@ import './NavBar.css';
 import CartWidget from '../cartWidget/cartWidget';
 import { Link } from 'react-router-dom';
 
-export default function Navbar(props) {
+export default function Navbar() {
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
